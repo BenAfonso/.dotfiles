f1537c0bcfc47fd51fabb9f23f997928519cfa48
Ben Afonso <ben@nightzookeeper.com>
2024-01-17 02:05:21 +0100

    feat: re-enable dadbod

--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -1,6 +1,7 @@
 local M = {
   {
     "kristijanhusak/vim-dadbod-ui",
+    enabled = true,
     dependencies = {
       { "tpope/vim-dadbod", lazy = true },
       { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "nosql" }, lazy = true },
