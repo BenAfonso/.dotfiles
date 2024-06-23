6f63ddc33f13f1d783f65fafa8cab4d407a65756
Ben Afonso <ben@nightzookeeper.com>
2024-06-23 03:51:23 +0200

    feat: update chat gpt key command

--- configs/nvim/lua/plugins/chat-gpt.lua
+++ configs/nvim/lua/plugins/chat-gpt.lua
@@ -1,21 +1,22 @@
 local M = {
   {
     enabled = true,
-    'jackMort/ChatGPT.nvim',
-    event = 'VeryLazy',
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
     config = function()
-      local home = vim.fn.expand '$HOME'
-      local path = os.getenv 'KEEPASS_FOLDER'
-      require('chatgpt').setup {
+      local home = vim.fn.expand("$HOME")
+      require("chatgpt").setup({
         -- api_host_cmd = "echo http://127.0.0.1:4000",
         -- api_key_cmd = "echo DUMMY",
-        api_key_cmd = 'keepassxc-cli show ' .. path .. '/terminal-secrets.kdbx gpt-neovim2 -a Password --no-password -k ' .. home .. '/kp.keyx -s',
-      }
+        api_key_cmd = "echo $(pass show keepass-noha | keepassxc-cli show "
+          .. home
+          .. "/.keepass/keepass-noha.kdbx gpt-neovim2 -a Password -s -q)",
+      })
     end,
     dependencies = {
-      'MunifTanjim/nui.nvim',
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
     },
   },
 }
