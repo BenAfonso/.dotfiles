941fe4266067e8dcb2849ccc7a3363adbd7c56a8
Ben Afonso <ben@nightzookeeper.com>
2024-06-23 03:45:21 +0200

    feat: add essentials

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -7,8 +7,14 @@ nix-env -iA nixpkgs.age
 nix-env -iA nixpkgs.btop
 nix-env -iA nixpkgs.duf
 nix-env -iA nixpkgs.eza
-nix-env -iA nixpkgs.fish
 nix-env -iA nixpkgs.jq
 nix-env -iA nixpkgs.keepassxc
 nix-env -iA nixpkgs.nix
 nix-env -iA nixpkgs.tealdeer
+
+nix-env -iA nixpkgs.ripgrep
+nix-env -iA nixpkgs.go
+nix-env -iA nixpkgs.fzf
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -sS https://starship.rs/install.sh | sh
