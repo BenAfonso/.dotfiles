f9cb88367b95dbd7bfa17fdd2dce99459e0b6a7b
Ben Afonso <ben@nightzookeeper.com>
2024-09-21 12:23:56 +0200

    Add gpt key command for gpt nvim

--- configs/nvim/lua/plugins/chat-gpt.lua
+++ configs/nvim/lua/plugins/chat-gpt.lua
@@ -5,12 +5,14 @@ local M = {
     event = "VeryLazy",
     config = function()
       local home = vim.fn.expand("$HOME")
+      -- local cmd = "echo $(pass show keepass-noha | keepassxc-cli show "
+      --   .. home
+      --   .. "/.keepass/keepass-noha.kdbx gpt-neovim2 -a Password -s -q)"
+
       require("chatgpt").setup({
         -- api_host_cmd = "echo http://127.0.0.1:4000",
         -- api_key_cmd = "echo DUMMY",
-        api_key_cmd = "echo $(pass show keepass-noha | keepassxc-cli show "
-          .. home
-          .. "/.keepass/keepass-noha.kdbx gpt-neovim2 -a Password -s -q)",
+        api_key_cmd = "gpt-key",
       })
     end,
     dependencies = {
