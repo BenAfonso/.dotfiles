0db833e02f6c6e60372c8760d4b884323ab8b8d1
Ben Afonso <ben@nightzookeeper.com>
2024-10-27 18:32:16 +0100

    update dashboard to be centered

--- configs/nvim/lua/plugins/dashboard.lua
+++ configs/nvim/lua/plugins/dashboard.lua
@@ -1,34 +1,38 @@
 local M = {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
   opts = function()
-    vim.cmd [[:hi DashboardHeader guifg=#ff007f]]
+    vim.cmd([[:hi DashboardHeader guifg=#ff007f]])
     local logo = [[
-        ██████╗ ███████╗███╗   ██╗    ██╗   ██╗██╗███╗   ███╗
-        ██╔══██╗██╔════╝████╗  ██║    ██║   ██║██║████╗ ████║
-        ██████╔╝█████╗  ██╔██╗ ██║    ██║   ██║██║██╔████╔██║
-        ██╔══██╗██╔══╝  ██║╚██╗██║    ╚██╗ ██╔╝██║██║╚██╔╝██║
-        ██████╔╝███████╗██║ ╚████║     ╚████╔╝ ██║██║ ╚═╝ ██║
-        ╚═════╝ ╚══════╝╚═╝  ╚═══╝      ╚═══╝  ╚═╝╚═╝     ╚═╝
+██████╗ ███████╗███╗   ██╗    ██╗   ██╗██╗███╗   ███╗
+██╔══██╗██╔════╝████╗  ██║    ██║   ██║██║████╗ ████║
+██████╔╝█████╗  ██╔██╗ ██║    ██║   ██║██║██╔████╔██║
+██╔══██╗██╔══╝  ██║╚██╗██║    ╚██╗ ██╔╝██║██║╚██╔╝██║
+██████╔╝███████╗██║ ╚████║     ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═════╝ ╚══════╝╚═╝  ╚═══╝      ╚═══╝  ╚═╝╚═╝     ╚═╝
+  .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.
+ / / \ \ / / \ \ / / \ \ / / \ \ / / \ \ / / \ \ / / 
+`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'  
       ]]
 
-    logo = string.rep('\n', 3) .. logo .. '\n\n'
+    logo = string.rep("\n", 3) .. logo .. "\n\n"
 
     local opts = {
-      theme = 'doom',
+      theme = "doom",
       hide = {
         -- this is taken care of by lualine
         -- enabling this messes up the actual laststatus setting after loading a file
         statusline = false,
       },
       config = {
-        header = vim.split(logo, '\n'),
+        header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+          -- { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+          -- { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "h" },
+          -- { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+          -- { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
           -- { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
           -- { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
           -- { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
@@ -36,25 +40,34 @@ local M = {
           { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
         },
         footer = function()
-          local stats = require('lazy').stats()
+          local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
         end,
       },
     }
 
+    if require("ben.is-git-repositiory")() then
+      table.insert(opts.config.center, {
+        action = "Git",
+        desc = " Git",
+        icon = "  ",
+        key = "l",
+      })
+    end
+
     for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
-      button.key_format = '  %s'
+      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+      button.key_format = "  %s"
     end
 
     -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == 'lazy' then
+    if vim.o.filetype == "lazy" then
       vim.cmd.close()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'DashboardLoaded',
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "DashboardLoaded",
         callback = function()
-          require('lazy').show()
+          require("lazy").show()
         end,
       })
     end
