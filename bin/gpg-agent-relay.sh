f8737daf9eef55ef95126a8e7b9302a6af0060dd
Ben Afonso <ben@nightzookeeper.com>
2025-04-22 22:20:13 +0200

    update gpg relay

--- bin/gpg-agent-relay.sh
+++ bin/gpg-agent-relay.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Launches socat+npiperelay to relay the gpg-agent socket file for use in WSL
-# See https://justyn.io/blog/using-a-yubikey-for-gpg-in-windows-10-wsl-windows-subsystem-for-linux/ for details
 
 GPGDIR="${HOME}/.gnupg"
 WIN_GPGDIR="C:/Users/Ben/AppData/Local/gnupg"
@@ -8,26 +6,22 @@ NPIPERELAY="/mnt/c/Users/Ben/go/bin/npiperelay.exe"
 PIDFILE="${GPGDIR}/.gpg-agent-relay.pid"
 OLDPID=$(cat "${PIDFILE}")
 
-# Launch socat+npiperelay for the regular gpg-agent
-if [ ! -z "${OLDPID}" ]; then
-    ps -p "${OLDPID}" >/dev/null && \
-    echo "gpg-agent-relay.sh already running with process id $(cat ${PIDFILE})" && \
-    exit 0
+
+ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ep -ei -s -a 'C:/Users/Ben/AppData/Local/gnupg/S.gpg-agent.ssh'"; echo $?)
+
+if [ $ALREADY_RUNNING != "0" ] ; then
+    if [ -S $GPGDIR/S.gpg-agent ]; then
+        # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
+        echo "removing previous socket..."
+        rm $GPGDIR/S.gpg-agent
+        rm $GPGDIR/S.gpg-agent.ssh
+    fi
+
+
+        echo "Starting gpg agent relay"
+        (setsid socat UNIX-LISTEN:"${GPGDIR}/S.gpg-agent,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GPGDIR}/S.gpg-agent'",nofork &) >/dev/null 2>&1
+        (setsid socat UNIX-LISTEN:"${GPGDIR}/S.gpg-agent.ssh,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GPGDIR}/S.gpg-agent.ssh'",nofork &) >/dev/null 2>&1
+
+else 
+    echo "Already running"
 fi
-
-
-rm -f "${GPGDIR}/S.gpg-agent*"
-
-echo $$ > ${PIDFILE}
-
-
-# Relay the regular gpg-agent socket for gpg operations
-socat UNIX-LISTEN:"${GPGDIR}/S.gpg-agent,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GPGDIR}/S.gpg-agent'",nofork &
-AGENTPID=$!
-
-# Relay the gpg ssh-agent
-socat UNIX-LISTEN:"${GPGDIR}/S.gpg-agent.ssh,fork" EXEC:"${NPIPERELAY} -ep -ei -s -a '${WIN_GPGDIR}/S.gpg-agent.ssh'",nofork &
-SSHPID=$!
-
-wait ${AGENTPID}
-wait ${SSHPID}
