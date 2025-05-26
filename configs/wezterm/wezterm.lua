f80f8bd1dbad5766119c661846f7b15c0f566437
Ben Afonso <ben@nightzookeeper.com>
2025-05-26 10:47:46 +0200

    update font to maple mono

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -13,13 +13,15 @@ config.font_size = 14
 config.line_height = 1
 
 -- config.font = wezterm.font("FiraCode Nerd Font")
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
+-- config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
+config.font = wezterm.font("Maple Mono NF", { weight = "Regular" })
 -- config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
 -- config.bold_brightens_ansi_colors = "BrightAndBold"
 
 -- config.font = wezterm.font("Monaspace Neon", { weight = "Regular" })
 -->
 -- => asdas
+--
 
 -- config.font_rules = {
 -- 	-- normal-intensity-and-italic
@@ -57,15 +59,15 @@ config.font_rules = {
 	-- 	}),
 	-- },
 	-- 	-- normal-intensity-and-italic
-	{
-		intensity = "Normal",
-		italic = true,
-		font = wezterm.font({
-			family = "Monaspace Radon",
-			weight = "Regular",
-			italic = false,
-		}),
-	},
+	-- {
+	-- 	intensity = "Normal",
+	-- 	italic = true,
+	-- 	font = wezterm.font({
+	-- 		family = "Monaspace Radon",
+	-- 		weight = "Regular",
+	-- 		italic = false,
+	-- 	}),
+	-- },
 }
 
 -- config.exit_behavior = "Close"
@@ -77,6 +79,7 @@ config.exit_behavior_messaging = "None"
 
 -- config.window_decorations = "RESIZE"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
 
 if string.find(wezterm.target_triple, "windows") ~= nil then
 	config.default_domain = "WSL:Ubuntu"
@@ -95,6 +98,15 @@ config.keys = {
 		mods = "CMD|SHIFT",
 		action = wezterm.action.ReloadConfiguration,
 	},
+	{
+		key = "y",
+		mods = "CTRL",
+		action = wezterm.action.SpawnTab({
+			DomainName = 'local',
+		}),
+	},
+	{ key = "O", mods = "CTRL",       action = wezterm.action.DisableDefaultAssignment },
+	{ key = "o", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "P", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "p", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "P", mods = "CTRL",       action = wezterm.action.DisableDefaultAssignment },
@@ -102,8 +114,17 @@ config.keys = {
 
 local color = "#ff33b8"
 
--- config.color_scheme = "Night Owl (Gogh)"
+
 config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.981
+-- config.window_background_opacity = 0.3
+config.macos_window_background_blur = 10
+-- config.window_frame = {
+-- 	active_titlebar_bg = "#011627",
+-- 	active_titlebar_fg = "#011627",
+-- 	inactive_titlebar_bg = "#061d30",
+-- 	inactive_titlebar_fg = "#060d30",
+-- }
 -- config.color_scheme = "Tokyo Night"
 
 -- config.window_background_opacity = 0.90
@@ -148,39 +169,37 @@ config.color_scheme = "Catppuccin Mocha"
 -- }
 
 -- Catpuccin
-config.window_frame = {
-	active_titlebar_bg = "#181825",
-	active_titlebar_fg = "#181825",
-	inactive_titlebar_bg = "#2a2b3c",
-	inactive_titlebar_fg = "#2a2b3c",
-}
+-- config.window_frame = {
+-- 	active_titlebar_bg = "#181825",
+-- 	active_titlebar_fg = "#181825",
+-- 	inactive_titlebar_bg = "#2a2b3c",
+-- 	inactive_titlebar_fg = "#2a2b3c",
+-- }
 config.colors = {
-	-- cursor_bg = "#80a4c2",
-	-- cursor_fg = "#7f6057",
 	cursor_bg = color,
 	cursor_fg = "#01121f",
-	tab_bar = {
-		active_tab = {
-			bg_color = "#181825",
-			fg_color = "#181825",
-		},
-		inactive_tab = {
-			bg_color = "#2a2b3c",
-			fg_color = "#5c7886",
-		},
-		inactive_tab_hover = {
-			bg_color = "#2a2b3c",
-			fg_color = "#c5ceeb",
-		},
-		new_tab = {
-			bg_color = "#181825",
-			fg_color = "#181825",
-		},
-		new_tab_hover = {
-			bg_color = "#2a2b3c",
-			fg_color = "#c5ceeb",
-		},
-	},
+	-- tab_bar = {
+	-- 	active_tab = {
+	-- 		bg_color = "#181825",
+	-- 		fg_color = "#181825",
+	-- 	},
+	-- 	inactive_tab = {
+	-- 		bg_color = "#2a2b3c",
+	-- 		fg_color = "#5c7886",
+	-- 	},
+	-- 	inactive_tab_hover = {
+	-- 		bg_color = "#2a2b3c",
+	-- 		fg_color = "#c5ceeb",
+	-- 	},
+	-- 	new_tab = {
+	-- 		bg_color = "#181825",
+	-- 		fg_color = "#181825",
+	-- 	},
+	-- 	new_tab_hover = {
+	-- 		bg_color = "#2a2b3c",
+	-- 		fg_color = "#c5ceeb",
+	-- 	},
+	-- },
 }
 
 -- Zen mode nvim plugin
